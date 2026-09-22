eba58be3be5166b65cb58f487428977c4c45aefa
zacky3006 <zacky3006@gmail.com>
2026-09-22 15:33:16 +0700

    create cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Patsapol"
+last_name = "Tungjaraspanichakul"
+print(first_name + " " + last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Patsapol"
+last_name = "Tungjaraspanichakul"
+whole_name = first_name + " " + last_name
+print(whole_name)
