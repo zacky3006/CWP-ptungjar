b486e11d103818a6018c3f7af8f6e0613a3cbdd9
zacky3006 <zacky3006@gmail.com>
2026-09-24 00:06:55 +0700

    update

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+first_name = input("Hey, what's your first name? :")
+last_name = input("And your last name? : ")
+whole_name = first_name + " " + last_name
+print("Well, pleased to meet you, " + whole_name + ".")
---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+iszero = int(input())
+if iszero == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
