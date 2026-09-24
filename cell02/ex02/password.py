8a501e8a46c000107068475ca895b6b9027cbf03
zacky3006 <zacky3006@gmail.com>
2026-09-24 12:49:58 +0700

    update 2.2 2.3

---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+password = "1234"
+input_password = input()
+
+if input_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+first = int(input("Enter the first number:\n"))
+second = int(input("Enter the second number:\n"))
+
+result = first * second
+
+print(f"{first} x {second} = {result}")
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
